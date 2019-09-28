3505d1408833bf4e7e05ca33a13756aeb677aa68
ullanor <pawel.nowacki95@gmail.com>
2019-09-28 21:49:36 +0200

    Dodaj pliki projektów.

---
+++ szczury/CharCounter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace szczury
+{
+    class CharCounter : IDisposable
+    {
+        string strToCount = string.Empty;
+        public CharCounter(string strToCount)
+        {
+            this.strToCount = strToCount;
+            Console.WriteLine("Creating object ...");
+
+            var charLookup = strToCount.ToUpper().Where(char.IsLetter).ToLookup(letter => letter);
+
+            foreach (var letter in charLookup)
+                Console.WriteLine("Char:{0} Count:{1}", letter.Key, charLookup[letter.Key].Count());
+
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("\nRemoving object ...\n");
+        }
+
+        //~CharCounter()
+        //{
+        //    Console.WriteLine("Finalizing object!");
+        //}
+    }
+}
---
+++ szczury/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace szczury
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWorking = true;
+            while (isWorking)
+            {
+                Console.WriteLine("Input number");
+                Console.WriteLine("6 - letters counting test");
+                short number = 99;
+                try
+                {
+                    number = Convert.ToInt16(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                switch (number)
+                {
+                    case 6:
+                        lettersCounting();
+                        break;
+                    case 0:
+                        Console.WriteLine("Closing");
+                        isWorking = false;
+                        break;
+                    case 1:
+                        break;
+                }
+            }
+        }
+
+        static void lettersCounting()
+        {
+            string test = "abbcc daba grabaz!! 123? A!";
+            Console.WriteLine("Counting ... \n{0}",test);
+            int abc = test.Count(char.IsLetter);
+            Console.WriteLine(abc);
+
+            CharCounter CC = new CharCounter(test);
+            //GC.Collect();
+        }
+    }
+}

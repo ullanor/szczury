8d9d57db6d4a96e3469ce3fec7916a80568b365d
MichalG-w <michalgrzegorzgajowy@gmail.com>
2019-10-01 20:45:04 +0200

    Download file from net

--- szczury/MultiCounterClass.cs
+++ szczury/MultiCounterClass.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Net;
 
 namespace szczury
 {
@@ -30,5 +31,18 @@ namespace szczury
         //locate your static methods here to make app looks cleaner ;)
 
         //TODO
+
+        public static void DownloadFileFromWeb()
+        {
+            Console.WriteLine("Download request processing!\n");
+            string remoteUri = "https://s3.zylowski.net/public/input/2.txt";
+
+            WebClient myWebClient = new WebClient();
+            Console.WriteLine("Downloading File \"{0}\" from \"{1}\" .......\n\n", "2.txt",
+                remoteUri.Substring(0, remoteUri.Length - 5));
+
+            myWebClient.DownloadFile(remoteUri, Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "//webText.txt");
+            Console.WriteLine("Successfully Downloaded File \"{0}\"\n", remoteUri);
+        }
     }
 }
